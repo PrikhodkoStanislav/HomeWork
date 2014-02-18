070ffb9725887d2e2ba496e07fbf167495ff95fd
PrikhodkoStanislav <prikhodko-stanislav@yandex.ru>
2014-02-19 00:32:57 +0400

    Commit with numbers 1, 2, 3 of homework 1

---
+++ II semestr/HomeWork1/Zadacha2 Fibonacci/Zadacha2 Fibonacci/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Tests:
+ * 1)
+ * Input number:
+5
+Fibonacci number = 5
+Для продолжения нажмите любую клавишу . . .
+ * 2)
+ * Input number:
+15
+Fibonacci number = 610
+Для продолжения нажмите любую клавишу . . .
+ * 3)
+ * Input number:
+25
+Fibonacci number = 75025
+Для продолжения нажмите любую клавишу . . .
+ * 4)
+ * Input number:
+10
+Fibonacci number = 55
+Для продолжения нажмите любую клавишу . . .
+ * 5)
+ * Input number:
+30
+Fibonacci number = 832040
+Для продолжения нажмите любую клавишу . . .
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha2_Fibonacci
+{
+    class Program
+    {
+        static int FibonacciNumber(int number)
+        {
+            if (number == 0)
+                return 0;
+            if (number == 1)
+                return 1;
+            return FibonacciNumber(number - 1) + FibonacciNumber(number - 2);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Fibonacci number = {0}", FibonacciNumber(number));
+        }
+    }
+}
---
+++ II semestr/HomeWork1/Zadacha3 Sort/Zadacha3 Sort/Program.cs
@@ -0,0 +1,94 @@
+﻿/*
+ * Tests:
+ * 1)
+ * Input length of array:
+5
+Input array:
+5 3 4 2 1
+Sorted array:
+1 2 3 4 5
+Для продолжения нажмите любую клавишу . . .
+ * 2)
+ * Input length of array:
+7
+Input array:
+1 5 2 4 7 8 3
+Sorted array:
+1 2 3 4 5 7 8
+Для продолжения нажмите любую клавишу . . .
+ * 3)
+ * Input length of array:
+3
+Input array:
+20 27 15
+Sorted array:
+15 20 27
+Для продолжения нажмите любую клавишу . . .
+ * 4)
+ * Input length of array:
+4
+Input array:
+4 2 3 18
+Sorted array:
+2 3 4 18
+Для продолжения нажмите любую клавишу . . .
+ * 5)
+ * Input length of array:
+5
+Input array:
+10 23 5 25 20
+Sorted array:
+5 10 20 23 25
+Для продолжения нажмите любую клавишу . . .
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadacha3_Sort
+{
+    class Program
+    {
+        static void swap(int[] array, int i, int j)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        static void BubbleSort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        swap(array, j, j + 1);
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input length of array:");
+            int length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input array:");
+            int[] arrayOfNumbers = new int[length];
+            string stringWithElements = Console.ReadLine();
+            string[] arrayOfElements = stringWithElements.Split(' ');
+            for (int i = 0; i < length; i++)
+            {
+                arrayOfNumbers[i] = Convert.ToInt32(arrayOfElements[i]);
+            }
+            BubbleSort(arrayOfNumbers);
+            Console.WriteLine("Sorted array:");
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("{0} ", arrayOfNumbers[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
