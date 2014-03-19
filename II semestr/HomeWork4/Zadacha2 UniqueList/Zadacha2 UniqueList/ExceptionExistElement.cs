0b5670b94ba14a480b671257cc5b4780f2fab354
PrikhodkoStanislav <prikhodko-stanislav@yandex.ru>
2014-03-19 22:50:36 +0400

    Unique list: exceptions in two classes.

---
+++ II semestr/HomeWork4/Zadacha2 UniqueList/Zadacha2 UniqueList/ExceptionExistElement.cs
@@ -0,0 +1,20 @@
+﻿namespace UniqueListNamespace
+{
+    using System;
+
+    /// <summary>
+    /// Exception that element already exist.
+    /// </summary>
+    [Serializable]
+    public class ExceptionExistElement : Exception
+    {
+        /// <summary>
+        /// Constructor exception with message:
+        /// "This element already exist!".
+        /// </summary>
+        public ExceptionExistElement()
+        {
+            Console.WriteLine("This element already exist!");
+        }
+    }
+}
---
+++ II semestr/HomeWork4/Zadacha2 UniqueList/Zadacha2 UniqueList/ExceptionNotThisElement.cs
@@ -0,0 +1,21 @@
+﻿namespace UniqueListNamespace
+{
+    using System;
+
+    /// <summary>
+    /// Excepion that this element is not in the list,
+    /// but we work with him.
+    /// </summary>
+    [Serializable]
+    public class ExceptionNotThisElement : Exception
+    {
+        /// <summary>
+        /// Constructor exception with message:
+        /// "This element is NOT in the list!".
+        /// </summary>
+        public ExceptionNotThisElement()
+        {
+            Console.WriteLine("This element is NOT in the list!");
+        }
+    }
+}
