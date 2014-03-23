c265dfa89d19e125300e37b8dc4fff34da127611
PrikhodkoStanislav <prikhodko-stanislav@yandex.ru>
2014-03-24 01:23:40 +0400

    Filter with comments to the method.

--- II semestr/HomeWork5/Zadacha2 FilterOnBoolFunction/Zadacha2 FilterOnBoolFunction/Function.cs
+++ II semestr/HomeWork5/Zadacha2 FilterOnBoolFunction/Zadacha2 FilterOnBoolFunction/Function.cs
@@ -8,6 +8,12 @@
     /// </summary>
     public class Function
     {
+        /// <summary>
+        /// Filter elements of the list on the bool function.
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="func"></param>
+        /// <returns></returns>
         public List<int> Filter(List<int> list, Func<int, bool> func)
         {
             return list.FindAll(i => func(i));
