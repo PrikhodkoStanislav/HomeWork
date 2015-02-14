fc6a4b786b4c6f8323f66d12d62147e436169a79
PrikhodkoStanislav <prikhodko-stanislav@yandex.ru>
2015-02-14 18:37:24 +0300

    First homework in the IV semestr: factorial and fibonacci on F#.

---
+++ IV semestr/HomeWork1/Zadacha1 Factorial/Zadacha1 Factorial/Program.fs
@@ -0,0 +1,4 @@
+﻿let number = 5
+// Formula for factorial of x.
+let rec factorial x = if x = 0 then 1 else x * factorial (x - 1)
+System.Console.WriteLine(factorial number)
---
+++ IV semestr/HomeWork1/Zadacha2 Fibonacci/Zadacha2 Fibonacci/Program.fs
@@ -0,0 +1,4 @@
+﻿let number = 10
+// Formula for fibonacci number.
+let rec fibonacci n = if n = 0 then 0 else if n = 1 then 1 else fibonacci (n - 1) + fibonacci (n - 2)
+System.Console.WriteLine(fibonacci number)
