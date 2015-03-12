7141809e9d374cfbc782614e0b581cf1f7d16508
PrikhodkoStanislav <prikhodko-stanislav@yandex.ru>
2015-03-13 00:02:32 +0300

    Homework 3 Tasks 5 and 6.

---
+++ IV semestr/HomeWork3/Task5 ParseTreeDiscriminatedUnion/Task5 ParseTreeDiscriminatedUnion/Program.fs
@@ -0,0 +1,22 @@
+﻿type Tree<'a> =
+    | Tree of char * Tree<'a> * Tree<'a>
+    | Tip of 'a
+
+let parseTree tree = 
+    let rec countParseTree tree =
+        match tree with
+        | Tree('+', l, r) -> countParseTree l + countParseTree r
+        | Tree('-', l, r) -> countParseTree l - countParseTree r
+        | Tree('*', l, r) -> countParseTree l * countParseTree r
+        | Tree('/', l, r) -> if (countParseTree r = 0) then
+                                 printfn("Error: division by zero!");
+                                 0
+                             else
+                                 countParseTree l / countParseTree r
+        | Tip x -> x
+        | Tree(_, _, _) -> 0
+    countParseTree tree
+
+let tree = Tree('+', Tree('-', Tip(2), Tip (1)), Tree('*', Tip(2), Tree('/', Tip(3), Tip(1))))
+
+parseTree tree
---
+++ IV semestr/HomeWork3/Task6 InfiniteSequenceOfPrimeNumbers/Task6 InfiniteSequenceOfPrimeNumbers/Program.fs
@@ -0,0 +1,6 @@
+﻿let isPrimeNumber n = not ( (Seq.exists (fun x -> n % x = 0) ) {2 .. n / 2 } )
+
+let generateInfiniteSequenceOfPrimeNumbers =
+     Seq.filter isPrimeNumber (Seq.initInfinite( fun i -> i + 2))
+
+printfn "%A" generateInfiniteSequenceOfPrimeNumbers
