d446d51521f7c0fa406e579ff7e64de67259b4af
PrikhodkoStanislav <prikhodko-stanislav@yandex.ru>
2015-03-13 01:27:15 +0300

    Homework 4 Tasks 1 and 2.

---
+++ IV semestr/HomeWork4/Task1 IsTrueBracketSequence/Task1 IsTrueBracketSequence/Program.fs
@@ -0,0 +1,32 @@
+﻿let isTrueBracketSequence theString =
+    let rec parseString theString index list =
+        if (index < String.length theString) then
+            match (theString.[index]) with
+            | '(' -> parseString theString (index + 1) (theString.[index] :: list)
+            | '{' -> parseString theString (index + 1) (theString.[index] :: list)
+            | '[' -> parseString theString (index + 1) (theString.[index] :: list)
+            | ')' -> if (List.head list = '(') then
+                         parseString theString (index + 1) (List.tail list)
+                     else
+                         false
+            | '}' -> if (List.head list = '{') then
+                         parseString theString (index + 1) (List.tail list)
+                     else
+                         false
+            | ']' -> if (List.head list = '[') then
+                         parseString theString (index + 1) (List.tail list)
+                     else
+                         false
+            | _ -> parseString theString (index + 1) list
+        else
+            list = []
+    parseString theString 0 []
+
+// true
+isTrueBracketSequence "{sddsfdf}"
+
+// false
+isTrueBracketSequence "{[sdd(sdsdfsdf]sdfsdf)sfdf}"
+
+// true
+isTrueBracketSequence "ololo[({([sdfdfsfsdfsdf(sf)sfsfsd[sf]sdf]sdfsdfsdf)sdfsd(s)qq}sfsd)sdfsfsdfsd[sf[sdf]sdf]sdf]qw"
---
+++ IV semestr/HomeWork4/Task2 PointFreeStyle/Task2 PointFreeStyle/Program.fs
@@ -0,0 +1,19 @@
+﻿let func x l = List.map (fun y -> y * x) l
+
+let func'01 x = List.map (fun y -> y * x)
+
+let func'02 x : int list -> int list = List.map (fun y -> y * x)
+
+let func'03 x : int list -> int list = List.map ((*) x)
+
+// [5; 10; 15; 20; 25]
+func 5 [1; 2; 3; 4; 5]
+
+// [5; 10; 15; 20; 25]
+func'01 5 [1; 2; 3; 4; 5]
+
+// [5; 10; 15; 20; 25]
+func'02 5 [1; 2; 3; 4; 5]
+
+// [5; 10; 15; 20; 25]
+func'03 5 [1; 2; 3; 4; 5]
