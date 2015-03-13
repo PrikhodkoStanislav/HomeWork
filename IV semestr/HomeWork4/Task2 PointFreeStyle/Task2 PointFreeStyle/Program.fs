a9a736bf95c0c23085f1d2414a1ef06fd4565967
PrikhodkoStanislav <prikhodko-stanislav@yandex.ru>
2015-03-13 20:29:21 +0300

    Homework 4 Point free style with one more iteration.

--- IV semestr/HomeWork4/Task2 PointFreeStyle/Task2 PointFreeStyle/Program.fs
+++ IV semestr/HomeWork4/Task2 PointFreeStyle/Task2 PointFreeStyle/Program.fs
@@ -6,6 +6,9 @@ let func'02 x : int list -> int list = List.map (fun y -> y * x)
 
 let func'03 x : int list -> int list = List.map ((*) x)
 
+let func'04 : int -> (int list -> int list) = (*) >> List.map
+
+
 // [5; 10; 15; 20; 25]
 func 5 [1; 2; 3; 4; 5]
 
@@ -17,3 +20,6 @@ func'02 5 [1; 2; 3; 4; 5]
 
 // [5; 10; 15; 20; 25]
 func'03 5 [1; 2; 3; 4; 5]
+
+// [5; 10; 15; 20; 25]
+func'04 5 [1; 2; 3; 4; 5]
