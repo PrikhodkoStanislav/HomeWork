3e1fdb0eb75c75df8f93e3e4c1f481f5cf0c044b
PrikhodkoStanislav <prikhodko-stanislav@yandex.ru>
2015-04-03 00:57:04 +0300

    Homework 6 Task 1 Workflow for rounding.

---
+++ IV semestr/HomeWork6/Task1 Workflow/Task1 Workflow/Program.fs
@@ -0,0 +1,14 @@
+﻿type rounding (exactitude : int) =
+    member this.Bind (x : float, rest : float -> float) =
+        rest x
+    member this.Return (x : float) =
+        System.Math.Round (x, exactitude)
+    
+
+// float = 0.048
+let roundingOperations = 
+    rounding 3 {
+        let! a = 2.0 / 12.0
+        let! b = 3.5
+        return a / b
+    }
