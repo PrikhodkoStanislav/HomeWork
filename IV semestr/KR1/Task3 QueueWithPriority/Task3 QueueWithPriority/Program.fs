314c9a19c778e94a256731ea19782954fc2a3588
PrikhodkoStanislav <prikhodko-stanislav@yandex.ru>
2015-04-03 12:55:47 +0300

    KR1 Task 3

---
+++ IV semestr/KR1/Task3 QueueWithPriority/Task3 QueueWithPriority/Program.fs
@@ -0,0 +1,10 @@
+﻿type QueueWithPriority (query : list<'a>, length : int) =
+    member q.Length = length
+    member q.Elements = query
+    member q.Push(element, priority) = QueueWithPriority((element, priority) :: q.Elements, q.Length + 1)
+//    member q.Pop() = if (q.Length = 0) then
+//                         printfn "%s" "Error!"
+//                     else
+//                         
+
+let q = QueueWithPriority([], 0)
