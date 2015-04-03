5f1909f68c8e8aae18a9f34a32a5d4153af3280e
PrikhodkoStanislav <prikhodko-stanislav@yandex.ru>
2015-04-03 19:16:23 +0300

    KR1 Task 2

--- IV semestr/KR1/Task2 BinaryTreeWIthFunc/Task2 BinaryTreeWIthFunc/Program.fs
+++ IV semestr/KR1/Task2 BinaryTreeWIthFunc/Task2 BinaryTreeWIthFunc/Program.fs
@@ -7,12 +7,14 @@ let filterTree func tree =
     let rec funcForElementOfTree func tree =
         match tree with
         | Tip x -> if (func x) then
-                       printfn "%A" x
+                       [x]
+                   else
+                       []
         | Tree(x, l, r) -> if (func x) then
-                               printfn "%A" x
-                           funcForElementOfTree func l
-                           funcForElementOfTree func r
+                               x :: (funcForElementOfTree func l) @ (funcForElementOfTree func r)
+                           else
+                               (funcForElementOfTree func l) @ (funcForElementOfTree func r)
     funcForElementOfTree func tree
 
-
+// int list = [2; 4]
 filterTree (fun x -> (x % 2) = 0) (Tree (2, Tip 1, Tip 4))
